d650e597c1e24300cadd5a500489b85d9f7cc0c9
OtabekAbdurakhmonov <77769473+OtabekAbdurakhmonov@users.noreply.github.com>
2021-08-16 21:07:11 +0900

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+var searchBox = document.getElementById("searchBox");
+var googleIcon = document.getElementById("myIcon");
+
+myIcon.onclick = function() {
+  searchBox.classList.toggle("active");
+}
